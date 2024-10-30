0fc2fb347ed6351626b15b2cbbb6f2d232fdff6e
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-10-30 09:03:58 +0400

    Correct Names and Remove Left Shift
    Replace left shift with right shift of number % 26, since that works.

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,4 +1,4 @@
-def shift_char_left(char, number)
+def shift_char_right(char, number)
   char = char.ord
   if ((char >= 65 && char <= 90 - number) ||
     (char >= 97 && char <= 122 - number))
@@ -15,24 +15,9 @@ def shift_char_left(char, number)
   char.chr
 end
 
-def shift_char_right(char, number)
-  char = char.ord
-  if ((char >= 65 + number && char <= 90) ||
-    (char >= 97 + number && char <= 122))
-    # these numbers are the ASCII codes for (LIMIT)-Z, and (limit)-z
-    char -= number
-  elsif ((char >= 65 && char <= 65  + number) ||
-    (char >= 97 && char <= 97  + number))
-    # for the ones that would go beyond "A" or "a"
-    char += 26
-    char -= number
-  else
-    char = char
-  end
-  char.chr
-end
-
 def caesar_cipher(string, number)
   string_array = string.split(//)
-  string_array.map {|char| shift_char(char, number)}.join("")
+  string_array.map {|char| shift_char_right(char, number % 26)}.join("")
+  # The % operator can take a negative number (for shift left)
+  # and turn it into the equivalent right shift
 end
