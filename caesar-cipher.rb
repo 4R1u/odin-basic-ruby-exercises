818e4066adf60988d94ce5245c6024a9e62a8bf1
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-10-30 08:46:06 +0400

    Reformat shift_char

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,9 +1,9 @@
 def shift_char(char, number)
   char = char.ord
   if ((char >= 65 && char <= 90 - number) ||
-     (char >= 97 && char <= 122 - number))
-     # these numbers are the ASCII codes for A-(LIMIT), and a-(limit)
-     char += number
+    (char >= 97 && char <= 122 - number))
+    # these numbers are the ASCII codes for A-(LIMIT), and a-(limit)
+    char += number
   elsif ((char >= 90 - number && char <= 90) ||
     (char >= 122 - number && char <= 122))
     # for the ones that would go beyond "Z" or "z"
