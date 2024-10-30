0820d6235222249a67f6ff9f5b06c7c16ffe1994
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-10-30 10:55:15 +0400

    Make substrings.rb More Idiomatic

--- substrings.rb
+++ substrings.rb
@@ -1,7 +1,9 @@
 def substrings(word, substrings)
   substrings.reduce(Hash.new(0)) do |hash, substring|
-    hash[substring] = word.downcase
-    .split(substring.downcase).length - 1
+    count = word.downcase.scan(substring.downcase).count
+    if count > 0
+      hash[substring] = count
+    end
     hash
   end
 end
