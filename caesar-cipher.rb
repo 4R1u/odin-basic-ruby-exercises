5567bf38ac2c25c4bd98fc0d8295716ef585ba1d
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-10-30 08:44:30 +0400

    Implement Basic shift_char Function With Working Left Shift

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,19 +1,22 @@
+def shift_char(char, number)
+  char = char.ord
+  if ((char >= 65 && char <= 90 - number) ||
+     (char >= 97 && char <= 122 - number))
+     # these numbers are the ASCII codes for A-(LIMIT), and a-(limit)
+     char += number
+  elsif ((char >= 90 - number && char <= 90) ||
+    (char >= 122 - number && char <= 122))
+    # for the ones that would go beyond "Z" or "z"
+    char -= 26
+    char += number
+  else
+    char = char
+  end
+  char
+end
+
+
 def caesar_cipher(string, number)
   string_array = string.split(//)
-  string_array.map do |char|
-    char = char.ord
-    if ((char >= 65 && char <= 90 - number) ||
-       (char >= 97 && char <= 122 - number))
-       # these numbers are the ASCII codes for A-(LIMIT), and a-(limit)
-       char += number
-    elsif ((char >= 90 - number && char <= 90) ||
-      (char >= 122 - number && char <= 122))
-      # for the ones that would go beyond "Z" or "z"
-      char -= 26
-      char += number
-    else
-      char = char
-    end
-    char
-  end.map(&:chr).join("")
+  string_array.map {|char| shift_char(char, number)}.map(&:chr).join("")
 end
