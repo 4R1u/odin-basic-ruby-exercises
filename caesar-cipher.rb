44f6e6a66934310b3586d13463b34b134bebbc2c
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-10-30 08:54:14 +0400

    Implement shift_char_right

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -15,6 +15,22 @@ def shift_char_left(char, number)
   char.chr
 end
 
+def shift_char_right(char, number)
+  char = char.ord
+  if ((char >= 65 + number && char <= 90) ||
+    (char >= 97 + number && char <= 122))
+    # these numbers are the ASCII codes for (LIMIT)-Z, and (limit)-z
+    char += number
+  elsif ((char >= 65 && char <= 65  + number) ||
+    (char >= 97 && char <= 97  + number))
+    # for the ones that would go beyond "A" or "a"
+    char += 26
+    char -= number
+  else
+    char = char
+  end
+  char.chr
+end
 
 def caesar_cipher(string, number)
   string_array = string.split(//)
