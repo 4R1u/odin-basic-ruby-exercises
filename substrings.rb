2e9ab707641e264a2f88464b228d5aba7dde9bef
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-10-30 10:34:23 +0400

    Implement Case-Insensitivity in substrings

--- substrings.rb
+++ substrings.rb
@@ -1,6 +1,6 @@
 def substrings(word, substrings)
-  substrings.reduce(Hash.new(0)) do |hash, substring|
-    if word.include?(substring)
+  substrings.downcase.reduce(Hash.new(0)) do |hash, substring|
+    if word.downcase.include?(substring)
       hash[substring] += 1
     end
     hash
