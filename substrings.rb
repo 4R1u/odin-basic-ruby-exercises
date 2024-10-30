6e1f539ff0ddbd73f6b992ec799144809fbf9896
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-10-30 10:46:17 +0400

    Handle Multiple Words and Substrings in substrings

--- substrings.rb
+++ substrings.rb
@@ -1,8 +1,10 @@
 def substrings(word, substrings)
   substrings.reduce(Hash.new(0)) do |hash, substring|
-    if word.downcase.include?(substring.downcase)
-      hash[substring] += 1
-    end
+    # if word.downcase.include?(substring.downcase)
+    #   hash[substring] += 1
+    # end
+    hash[substring] = word.downcase
+    .split(substring.downcase).length - 1
     hash
   end
 end
