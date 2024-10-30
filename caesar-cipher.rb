d2cd7ce1b488f14ec7ab95f3547b5821a2571c30
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-10-30 08:36:26 +0400

    Implement Caesar Cipher With Working Left Shift
    Also implement right shift that fails when number goes further behind
    "A" and "a".

---
+++ caesar-cipher.rb
@@ -0,0 +1,19 @@
+def caesar_cipher(string, number)
+  string_array = string.split(//)
+  string_array.map do |char|
+    char = char.ord
+    if ((char >= 65 && char <= 90 - number) ||
+       (char >= 97 && char <= 122 - number))
+       # these numbers are the ASCII codes for A-(LIMIT), and a-(limit)
+       char += number
+    elsif ((char >= 90 - number && char <= 90) ||
+      (char >= 122 - number && char <= 122))
+      # for the ones that would go beyond "Z" or "z"
+      char -= 26
+      char += number
+    else
+      char = char
+    end
+    char
+  end.map(&:chr).join("")
+end
