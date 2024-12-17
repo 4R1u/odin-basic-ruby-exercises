ca4e77a1954d0ffb6b118a7ad494147a75dfe2ca
4R1u <62248461+4R1u@users.noreply.github.com>
2024-12-17 10:51:04 +0400

    test: Shifts Chars Beyond Z

--- spec/caesar-cipher-spec.rb
+++ spec/caesar-cipher-spec.rb
@@ -5,5 +5,9 @@ describe '#shift_char_right' do
     it "returns 'd'" do
       expect(shift_char_right('a', 3)).to eq('d')
     end
+
+    it "returns 'b'" do
+      expect(shift_char_right('y', 3)).to eq('b')
+    end
   end
 end
